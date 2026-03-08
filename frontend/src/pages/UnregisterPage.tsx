fa06b3dcdfa8d8792ba68ad62e1680899360c882
gernotschulmeister <gernotschulmeister@gmx.at>
2026-03-08 17:02:29 +0100

    add other components to unregister page

--- frontend/src/pages/UnregisterPage.tsx
+++ frontend/src/pages/UnregisterPage.tsx
@@ -1,14 +1,16 @@
+import { Navigation } from "@/components/Navigation";
 import { UnRegistration } from "@/components/sections/UnRegistration";
 import { Footer } from "@/components/sections/Footer";
 
 export default function UnregisterPage() {
 
   return (
-    <div className="flex min-h-screen items-center justify-center bg-muted">
-      <div className="text-center">
-        <UnRegistration />
-        <Footer />
-      </div>
+
+    <div className="min-h-screen code-pattern-bg">
+      <Navigation />
+      <UnRegistration />
+      <Footer />
     </div>
+
   );
 }
