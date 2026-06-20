5e0b4c9b1f0344e62d85ffaa47f1dc21bc1469c9
gernotschulmeister <gernotschulmeister@gmx.at>
2026-06-20 11:05:52 +0200

    adapt wording for Java barcamp

--- backend/src/main/java/com/schulmeister/barcamp/registration/MailService.java
+++ backend/src/main/java/com/schulmeister/barcamp/registration/MailService.java
@@ -57,10 +57,10 @@ public class MailService {
             MimeMessageHelper helper = new MimeMessageHelper(message, true);
 
             helper.setTo(registration.getEmail());
-            helper.setSubject("Dev Barcamp Frankfurt Unregister");
+            helper.setSubject("Java Barcamp Frankfurt Unregister");
 
             helper.setText("""
-                <h2>Thank you for unregistering from the Developer Barcamp in Frankfurt</h2>
+                <h2>Thank you for unregistering from the Java Barcamp in Frankfurt</h2>
                 <a href="%s">Click here to unregister</a>
                 """.formatted(confirmUrl), true);
 
